fb3ecbd9ce268eccb13477931300656c37947c51
DESKTOP-L98P01V\dinhbaotram06 <truongdl@nichietsuvn.com>
2025-07-31 15:48:24 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <iframe
+        src="https://m-dev.zap.vn/"
+        title="react-zalo-embed"
+        frameBorder="0"
+        // allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        // allowFullScreen
+        style={{ width: '100%', height: '100vh' }}
+      ></iframe>
     </div>
   );
 }
